1e1bf2e28f1381f8353400cf0ffd2f39efc9e4cf
jozdk <djannikhaezo@web.de>
2022-02-27 23:55:45 +0100

    Set up morgan and cookie-parser middleware

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const path = require("path");
+
+const indexRouter = require("./routes/home.js");
 
 const app = express();
 
+// Set application properties
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
 
-// Set static folder
+// Mount middleware
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+// app.use("/fibonacci", fibonacciRouter);
 
 const PORT = process.env.PORT || 3000;
 
