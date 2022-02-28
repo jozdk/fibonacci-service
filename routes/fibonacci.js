902534d57ceb5aa56c282c42113b7ee0b354225f
jozdk <djannikhaezo@web.de>
2022-02-28 23:42:32 +0100

    Handle input errors

--- routes/fibonacci.js
+++ routes/fibonacci.js
@@ -4,12 +4,19 @@ const math = require("../math.js");
 
 router.get("/", (req, res, next) => {
     if (req.query.fibonum) {
-        // Calculate Fibonacci number
-        res.render("fibonacci", {
-            title: "Calculate Fibonacci numbers",
-            fibonum: req.query.fibonum,
-            fiboval: math.fibonacci(req.query.fibonum)
-        });
+        if (req.query.fibonum >= 0) {
+            res.render("fibonacci", {
+                title: "Calculate Fibonacci numbers",
+                fibonum: req.query.fibonum,
+                fiboval: math.fibonacci(req.query.fibonum)
+            });
+        } else {
+            res.render("fibonacci", {
+                title: "Calculate Fibonacci numbers",
+                inputError: true
+            });
+        }
+
     } else {
         res.render("fibonacci", {
             title: "Calculate Fibonacci numbers",
