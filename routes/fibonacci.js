b96f1533e68b7383933e6e30c6adb2deebbc5ad4
jozdk <djannikhaezo@web.de>
2022-03-01 18:22:34 +0100

    Look into req.query object in context of hbs rendering

--- routes/fibonacci.js
+++ routes/fibonacci.js
@@ -3,23 +3,30 @@ const router = express.Router();
 const math = require("../math.js");
 
 router.get("/", (req, res, next) => {
+    const title = "Calculate Fibonacci numbers"
+    // const { fibonum } = req.query.fibonum; // Like this?
+    // const fibonum = req.query.fibonum;
+    // const fibilibus = 2;
+    // console.log("whole object: ", fibonum);
+    // console.log("destructured value: ", {fibonum, fibilibus});
     if (req.query.fibonum) {
         if (req.query.fibonum >= 0) {
+
             res.render("fibonacci", {
-                title: "Calculate Fibonacci numbers",
-                fibonum: req.query.fibonum,
+                title: title,
+                fibonum: req.query.fibonum, // is this dangerous?
                 fiboval: math.fibonacci(req.query.fibonum)
             });
+
         } else {
             res.render("fibonacci", {
-                title: "Calculate Fibonacci numbers",
+                title: title,
                 inputError: true
             });
         }
-
     } else {
         res.render("fibonacci", {
-            title: "Calculate Fibonacci numbers",
+            title: title,
             fiboval: undefined
         });
     }
