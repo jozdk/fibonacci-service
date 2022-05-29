637f65aa150954db41e8ee9e05daa29710fc78fd
jozdk <djannikhaezo@web.de>
2022-05-30 01:33:15 +0200

    Take test algorithm from terminal

--- fibotimes.js
+++ fibotimes.js
@@ -1,21 +1,35 @@
 const math = require("./math");
 
-// for (let num = 1; num < 8000; num++) {
-//     let now = new Date().toISOString();
-//     console.log(`${now} Fibonacci for ${num} = ${math.fibonacci(num)}`);
-// }
+const argAlgorithm = process.argv[2];
 
-(async () => {
-    for (let num = 1; num <= 20; num++) {
-        await new Promise((resolve, reject) => {
-            math.fibonacciAsync(num, (err, fibo) => {
-                if (err) reject(err);
-                else {
-                    let now = new Date().toISOString();
-                    console.log(`${now} Fibonacci for ${num} = ${fibo}`);
-                    resolve();
-                }
-            })
-        })
+if (argAlgorithm === "recursion") {
+    console.log("Recursion");
+    for (var num = 1; num <= 40; num++) {
+        let now = new Date().toISOString();
+        console.log(`${now} Fibonacci for ${num} = ${math.fibonacci(num)}`);
     }
-})().catch((err) => console.log(err));
+} else if (argAlgorithm === "loop") {
+    console.log("Loop");
+    for (let num = 1; num <= 1000; num++) {
+        let now = new Date().toISOString();
+        console.log(`${now} Fibonacci for ${num} = ${math.fibonacciLoop(num)}`);
+    }
+} else if (argAlgorithm === "async-recursion") {
+    console.log("Async-recursion");
+    (async () => {
+        for (let num = 1; num <= 40; num++) {
+            await new Promise((resolve, reject) => {
+                math.fibonacciAsync(num, (err, fibo) => {
+                    if (err) reject(err);
+                    else {
+                        let now = new Date().toISOString();
+                        console.log(`${now} Fibonacci for ${num} = ${fibo}`);
+                        resolve();
+                    }
+                })
+            })
+        }
+    })().catch((err) => console.log(err));
+} else {
+    console.log("No argument provided");
+}
